69bf5c6ee665126994a391b3fbdb51cbafde9262
Vinod Kumar <vinodkumar1992>
2018-09-24 08:13:55 +0530

    Add project files.

---
+++ SpecFlowPractice/Features/Login_Feature.feature
@@ -0,0 +1,20 @@
+﻿Feature: Login_Feature
+	In order to access my account
+	As a user of the website
+	I want to log into the website
+
+@mytag
+Scenario Outline: Successfull login with valid credentials
+	Given User is at Home page
+	And Navigate to Login page	
+	When User enter <UserName> and <Password>
+	And clicks on login button
+	Then Successful LogIn message should display
+Examples: 
+| UserName       | Password  |
+| vinod3404      | vinod9030 |
+| vasundhara3404 | vinod3404 |
+
+Scenario: Successful Logout
+	When User clicks on logout
+	Then Successful LogOut message should display
---
+++ SpecFlowPractice/Steps/Login_Steps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlowPractice.Steps
+{
+    [Binding]
+    public class Login_Steps
+    {
+        IWebDriver driver;
+        [Given(@"User is at Home page")]
+        public void GivenUserIsAtHomePage()
+        {
+            driver = new ChromeDriver(@"C:\Users\Vinod Kumar\Documents\Visual Studio 2015\Projects\SpecFlowPractice\packages");
+            driver.Url = "http://www.store.demoqa.com";
+            driver.Manage().Window.Maximize();
+        }
+        
+        [Given(@"Navigate to Login page")]
+        public void GivenNavigateToLoginPage()
+        {
+            driver.FindElement(By.XPath(".//*[@id='account']/a")).Click();
+        }
+
+        [When(@"User enter (.*) and (.*)")]
+        public void WhenUserEnterAnd(string UserName, string Password)
+        {
+            driver.FindElement(By.XPath("//input[@id='log']")).SendKeys(UserName);
+            driver.FindElement(By.XPath("//input[@id='pwd']")).SendKeys(Password);
+        }
+        
+        [When(@"clicks on login button")]
+        public void WhenClicksOnLoginButton()
+        {
+            driver.FindElement(By.Id("login")).Click();
+        }
+        
+        [When(@"User clicks on logout")]
+        public void WhenUserClicksOnLogout()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Successful LogIn message should display")]
+        public void ThenSuccessfulLogInMessageShouldDisplay()
+        {
+            System.Threading.Thread.Sleep(2000);         
+            true.Equals(driver.FindElement(By.XPath("//*[@title='Logout']")).Displayed);
+            driver.Close();
+        }
+        
+        [Then(@"Successful LogOut message should display")]
+        public void ThenSuccessfulLogOutMessageShouldDisplay()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
